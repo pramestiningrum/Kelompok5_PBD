ba4abc3c563ab31a4a1f09499c6c13bf941fadc1
LaelatulHodijah <hodijah2208@gmail.com>
2023-09-22 15:07:55 +0700

    3

---
+++ .idea/Config/Database.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Config{
+    class Database{
+
+        static function getConnection(): \PDO
+        {
+            $host = "localhost";
+            $port = 3306;
+            $database = "db_todolist";
+            $username = "root";
+            $password = "";
+
+            return new \PDO("mysql:host=$host:$port;dbnames=$database", $username, $password);
+        }
+    }
+}
